25be82d7cd2517e1c567b88f730215775454e9b3
ItzStanleex <ItzStanleex@users.noreply.github.com>
2026-04-02 00:59:28 +0200

    Add optimized Minecraft Docker images (Debian Bookworm)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ cd /home/container
 # Output current Java version
 java -version
 
+# Force UTF-8 encoding for Java
+export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF-8 -Dconsole.encoding=UTF-8"
+
 # Replace startup variables
 MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
